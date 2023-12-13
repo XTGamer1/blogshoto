84c19b3be68de0929daa4575a91ad3c9ebca658c
shoto <mohitshedge1@gmail.com>
2023-12-14 04:18:20 +0530

    first commit :tada:

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kenma's blog
---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // the name of your website
+  title: 'Kenma',
+  // the description of the website (eg. what shows on Google)
+  description: "Kenma's blog",
+  // a short bio shown at the bottom of your blog posts
+  // It should complete the sentence: Written by Kenma ...
+  shortBio: '',
+  // a longer bio showing on the landing page of the blog
+  bio: ``,
+  author: 'Kenma',
+  githubUrl: 'https://github.com/XTGamer1/blogshoto',
+  // replace this by the url where your website will be published
+  siteUrl: 'http://localhost:8000',
+  social: {
+    // leave the social media you do not want to appear as empty strings
+    twitter: '',
+    medium: '@kenmahosting',
+    facebook: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+  },
+  // GoatCounter code to enable analytics. See https://github.com/mathieudutour/medium-to-own-blog/tree/master/docs#analytics-tracking-page-views-etc
+  goatCounterCode: null,
+}
